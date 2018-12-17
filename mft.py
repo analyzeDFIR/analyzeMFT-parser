fc907a815ef37693b900a3cf5909c4dda23b84dd
analyzeDFIR <a.dfir@sudomail.com>
2018-12-17 15:59:15 -0500

    updated dependency modules

--- mft.py
+++ mft.py
@@ -52,44 +52,44 @@ class MFTEntryAttribute(ByteParser):
     def _parse_logged_utility_stream(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             None
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         return None
     def _parse_bitmap(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             None
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         return None
     def _parse_index_root(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Any>
             MFT entry index root attribute (see: structures.index)
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         index_root = Container()
         index_root.root_header = mftstructs.MFTIndexRootHeader.parse_stream(stream)
-        node_header_position = stream.tell()
+        node_header_position = self.stream.tell()
         index_root.node_header = mftstructs.MFTIndexNodeHeader.parse_stream(stream)
         index_root.entries = list()
-        stream.seek(node_header_position + index_root.node_header.IndexValuesOffset)
-        index_entry_start_position = stream.tell()
+        self.stream.seek(node_header_position + index_root.node_header.IndexValuesOffset)
+        index_entry_start_position = self.stream.tell()
         index_entry_size = index_root.node_header.IndexNodeSize - mftstructs.MFTIndexNodeHeader.sizeof()
-        while (stream.tell() - index_entry_start_position) < index_entry_size:
+        while (self.stream.tell() - index_entry_start_position) < index_entry_size:
             try:
-                index_entry_position = stream.tell()
+                index_entry_position = self.stream.tell()
                 index_entry = mftstructs.MFTIndexEntry.parse_stream(stream)
                 seek_length = mftstructs.MFTIndexEntry.sizeof() + \
                     index_entry.IndexValueSize + \
@@ -100,69 +100,69 @@ class MFTEntryAttribute(ByteParser):
                 if index_entry.Flags.IS_LAST:
                     break
                 else:
-                    stream.seek(index_entry_position + seek_length)
+                    self.stream.seek(index_entry_position + seek_length)
             except:
                 break
         return self._clean_value(index_root)
     def _parse_data(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Any>
             MFT entry resident data attribute
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
-        data = Container(content=stream.read(self.header.RecordLength))
+        data = Container(content=self.stream.read(self.header.RecordLength))
         data.sha2hash = hashlib.sha256(data.content).hexdigest()
         return data
     def _parse_volume_information(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Any>
             MFT entry volume information attribute (see: structures.volume_information)
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         return self._clean_value(mftstructs.MFTVolumeInformation.parse_stream(stream))
     def _parse_volume_name(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             String
             MFT entry volume name attribute (see: structures.volume_name)
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
-        return stream.read(self.header.Form.ValueLength).decode('UTF16')
+        return self.stream.read(self.header.Form.ValueLength).decode('UTF16')
     def _parse_access_control_list(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Container<String, Any>>
             MFT entry access control list attribute (see: structures.general.access_control_list)
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         try:
             acl = Container()
             acl.header = mftstructs.NTFSACLHeader.parse_stream(stream)
-            acl_position = stream.tell()
+            acl_position = self.stream.tell()
             acl_size = acl.header.AclSize - mftstructs.NTFSACLHeader.sizeof()
             acl.body = list()
-            while (stream.tell() - acl_position) < acl_size:
-                ace_position = stream.tell()
+            while (self.stream.tell() - acl_position) < acl_size:
+                ace_position = self.stream.tell()
                 try:
                     ace = Container()
                     ace.header = mftstructs.NTFSACEHeader.parse_stream(stream)
                     ace.body = mftstructs.NTFSACEAcessMask.parse_stream(stream)
                     acl.body.append(ace)
-                    stream.seek(ace_position + ace.header.AceSize)
+                    self.stream.seek(ace_position + ace.header.AceSize)
                 except:
                     break
             return self._clean_value(acl)
@@ -171,32 +171,32 @@ class MFTEntryAttribute(ByteParser):
     def _parse_security_descriptor(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Any>
             MFT entry security descriptor attribute (see: structures.security_descriptor)
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
-        header_position = stream.tell()
+        header_position = self.stream.tell()
         security_descriptor = Container(
             header=None,
             body=Container()
         )
         security_descriptor.header = mftstructs.MFTSecurityDescriptorHeader.parse_stream(stream)
         if security_descriptor.header.Control.SE_SELF_RELATIVE:
-            stream.seek(header_position + security_descriptor.header.OwnerSIDOffset)
+            self.stream.seek(header_position + security_descriptor.header.OwnerSIDOffset)
             security_descriptor.body.OwnerSID = mftstructs.NTFSSID.parse_stream(stream)
-            stream.seek(header_position + security_descriptor.header.GroupSIDOffset)
+            self.stream.seek(header_position + security_descriptor.header.GroupSIDOffset)
             security_descriptor.body.GroupSID = mftstructs.NTFSSID.parse_stream(stream)
             if security_descriptor.header.Control.SE_SACL_PRESENT:
-                stream.seek(header_position + security_descriptor.header.SACLOffset)
-                security_descriptor.body.SACL = self._parse_access_control_list(stream=stream)
+                self.stream.seek(header_position + security_descriptor.header.SACLOffset)
+                security_descriptor.body.SACL = self._parse_access_control_list()
             else:
                 security_descriptor.body.SACL = None
             if security_descriptor.header.Control.SE_DACL_PRESENT:
-                stream.seek(header_position + security_descriptor.header.DACLOffset)
-                security_descriptor.body.DACL = self._parse_access_control_list(stream=stream)
+                self.stream.seek(header_position + security_descriptor.header.DACLOffset)
+                security_descriptor.body.DACL = self._parse_access_control_list()
             else:
                 security_descriptor.body.DACL = None
         else:
@@ -208,66 +208,66 @@ class MFTEntryAttribute(ByteParser):
     def _parse_object_id(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Any>
             MFT entry object id attribute (see: structures.object_id)
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         return self._clean_value(mftstructs.MFTObjectID.parse_stream(stream))
     def _parse_file_name(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Any>
             MFT entry file name attribute (see: structures.file_name)
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         file_name = mftstructs.MFTFileNameAttribute.parse_stream(stream)
         for field in file_name:
             if field.startswith('Raw') and field.endswith('Time'):
                 file_name[field.replace('Raw', '')] = WindowsTime(file_name[field]).parse()
-        file_name.FileName = stream.read(file_name.FileNameLength * 2).decode('UTF16')
+        file_name.FileName = self.stream.read(file_name.FileNameLength * 2).decode('UTF16')
         return self._clean_value(file_name)
     def _parse_attribute_list(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Any>
             MFT entry attribute list attribute (see: structures.attribute_list)
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         attributes = Container()
-        while stream.tell() < self.header.Form.ValueLength:
-            AL_original_position = stream.tell()
+        while self.stream.tell() < self.header.Form.ValueLength:
+            AL_original_position = self.stream.tell()
             try:
                 attribute_list_entry = mftstructs.MFTAttributeListEntry.parse_stream(stream)
                 if attribute_list_entry.AttributeTypeCode == 'END_OF_ATTRIBUTES':
                     break
-                stream.seek(AL_original_position + attribute_list_entry.AttributeNameOffset)
-                attribute_list_entry.AttributeName = stream.read(attribute_list_entry.AttributeNameLength * 2).decode('UTF16')
+                self.stream.seek(AL_original_position + attribute_list_entry.AttributeNameOffset)
+                attribute_list_entry.AttributeName = self.stream.read(attribute_list_entry.AttributeNameLength * 2).decode('UTF16')
             except:
                 break
             else:
                 if attribute_list_entry.AttributeTypeCode.lower() not in attributes:
                     attributes[attribute_list_entry.AttributeTypeCode.lower()] = list()
                 attributes[attribute_list_entry.AttributeTypeCode.lower()].append(attribute_list_entry)
-                stream.seek(AL_original_position + attribute_list_entry.RecordLength)
+                self.stream.seek(AL_original_position + attribute_list_entry.RecordLength)
         return self._clean_value(attributes)
     def _parse_standard_information(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Any>
             MFT entry standard information attribute (see: structures.standard_information)
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         standard_information = mftstructs.MFTStandardInformationAttribute.parse_stream(stream)
         for field in standard_information:
@@ -277,41 +277,41 @@ class MFTEntryAttribute(ByteParser):
     def _parse_body(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Any>
             MFT entry attribute information
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
-        original_position = stream.tell()
+        original_position = self.stream.tell()
         try:
             if self.header.FormCode != 0:
                 return None
             parser = '_parse_%s'%self.header.TypeCode.lower()
             if not (hasattr(self, parser) and callable(getattr(self, parser))):
                 return None
-            stream.seek(original_position + self.header.Form.ValueOffset)
-            return self._clean_value(getattr(self, parser)(stream=stream))
+            self.stream.seek(original_position + self.header.Form.ValueOffset)
+            return self._clean_value(getattr(self, parser)())
         finally:
-            stream.seek(original_position + self.header.RecordLength)
+            self.stream.seek(original_position + self.header.RecordLength)
     def _parse_header(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Any>
             MFT entry attribute header information (see: structures.headers)
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
-        original_position = stream.tell()
+        original_position = self.stream.tell()
         try:
             header = mftstructs.MFTAttributeHeader.parse_stream(stream)
             if header.NameLength > 0:
                 try:
-                    stream.seek(original_position + header.NameOffset)
-                    header.Name = stream.read(header.NameLength * 2).decode('UTF16')
+                    self.stream.seek(original_position + header.NameOffset)
+                    header.Name = self.stream.read(header.NameLength * 2).decode('UTF16')
                 except Exception as e:
                     Logger.error('Failed to get name of attribute from header (%s)'%str(e))
                     header.Name = None
@@ -319,7 +319,7 @@ class MFTEntryAttribute(ByteParser):
                 header.Name = None
             return self._clean_value(header)
         finally:
-            stream.seek(original_position)
+            self.stream.seek(original_position)
 
 class MFTEntry(ByteParser):
     '''
@@ -331,23 +331,23 @@ class MFTEntry(ByteParser):
     def _parse_attributes(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Tuple<String, Container<String, Any>>
             Attributes in MFT entry
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         attributes = Container()
-        stream.seek(self.header.FirstAttributeOffset)
-        while stream.tell() < self.header.UsedSize:
-            original_position = stream.tell()
+        self.stream.seek(self.header.FirstAttributeOffset)
+        while self.stream.tell() < self.header.UsedSize:
+            original_position = self.stream.tell()
             type_code = mftstructs.MFTAttributeTypeCode.parse_stream(stream)
             if type_code is None or type_code == 'END_OF_ATTRIBUTES':
                 break
             type_code = type_code.lower()
-            stream.seek(original_position)
-            attribute = MFTEntryAttribute(stream.getvalue()[stream.tell():], stream=stream)
+            self.stream.seek(original_position)
+            attribute = MFTEntryAttribute(self.stream.getvalue()[self.stream.tell():], stream=self.stream)
             attribute.parse()
             if attribute.header is None:
                 continue
@@ -361,12 +361,12 @@ class MFTEntry(ByteParser):
     def _parse_header(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Container<String, Any>
             MFT entry attribute header information (see: structures.headers)
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         header = mftstructs.MFTEntryHeader.parse_stream(self.stream)
         if header.MultiSectorHeader.RawSignature == 0x454c4946:
@@ -386,12 +386,12 @@ class MFT(FileParser):
     def _parse_records(self):
         '''
         Args:
-            stream: TextIOWrapper|BufferedReader|BytesIO   => the stream to parse from
+            N/A
         Returns:
             Gen<MFTEntry>
             Generator of MFT entries
         Preconditions:
-            stream is of type TextIOWrapper, BufferedReader or BytesIO  (assumed True)
+            N/A
         '''
         record = self.stream.read(1024)
         while record != b'':
